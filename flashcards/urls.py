e004b36ffbe9a51b2722f5acc4fb50f5046a6603
Andrii <workerandrii@example.com>
2026-05-30 23:04:05 +0300

    Add deck list view

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, DeckListView
 
 urlpatterns = [
+    path("", DeckListView.as_view(), name="deck-list"),
     path("register/", RegisterView.as_view(), name="register"),
 ]
--- flashcards/views.py
+++ flashcards/views.py
@@ -1,9 +1,24 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+from django.views.generic import ListView
 from django.views.generic.edit import CreateView
+
+from .models import Deck
+
+
+class DeckListView(LoginRequiredMixin, ListView):
+    model = Deck
+    template_name = "flashcards/deck_list.html"
+    context_object_name = "decks"
+
+    def get_queryset(self):
+        return Deck.objects.filter(owner=self.request.user)  
+
 
 
 class RegisterView(CreateView):
     form_class = UserCreationForm
     template_name = "registration/register.html"
     success_url = reverse_lazy("login")
+

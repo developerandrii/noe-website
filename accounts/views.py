fb44b8bf2796dddff9cc6979a459085bf4a8f29e
Andrii <workerandrii@example.com>
2026-06-21 00:41:05 +0300

    Move registration to accounts app

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+
+# ----- AUTHENTICATIONS VIEWS -----
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = "accounts/register.html"
+    success_url = reverse_lazy("login")
--- flashcards/urls.py
+++ flashcards/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Authentication
-    path("register/", views.RegisterView.as_view(), name="register"),
-    # Decks
+   # Decks
     path("",  views.DeckListView.as_view(), name="deck-list"),
     path("decks/create/", views.CreateDeckView.as_view(), name="deck-create"),
     path("decks/<int:pk>/", views.DeckDetailView.as_view(), name="deck-detail"),

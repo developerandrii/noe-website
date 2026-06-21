3653df679cab9796f57858e32e9082351754ad7a
Andrii <workerandrii@example.com>
2026-06-21 21:09:27 +0300

    Add profile page

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,10 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
+
+from flashcards.models import Card
 
 
 # ----- AUTHENTICATIONS VIEWS -----
@@ -15,8 +19,28 @@ class AccountLogoutView(LogoutView):
 
 
 
-
 class RegisterView(CreateView):
     form_class = UserCreationForm
     template_name = "accounts/register.html"
     success_url = reverse_lazy("login")
+
+
+
+# ----- ACCOUNT VIEWS -----
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = "accounts/profile.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Show simple learning statistics for the logged-in user.
+        context["deck_count"] = self.request.user.decks.count()
+        context["card_count"] = Card.objects.filter(
+            deck__owner=self.request.user,
+        ).count()
+        context["learned_card_count"] = Card.objects.filter(
+            deck__owner=self.request.user,
+            is_learned=True,
+        ).count()
+
+        return context

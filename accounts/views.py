2803588bb7dbd93d354945b81d74258821533943
Andrii <workerandrii@example.com>
2026-06-21 15:14:52 +0300

    Move login to accounts app

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,15 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
 
 # ----- AUTHENTICATIONS VIEWS -----
+class AccountLoginView(LoginView):
+    template_name = "accounts/login.html"
+
+
+
 class RegisterView(CreateView):
     form_class = UserCreationForm
     template_name = "accounts/register.html"
